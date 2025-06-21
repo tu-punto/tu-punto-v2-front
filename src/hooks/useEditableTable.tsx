2939b86025d393d58ca53ddc5852f9168d069082
Andrezu <sanchezandres8203@gmail.com>
2025-06-21 16:06:07 -0400

    fix: utilidad increase on cantidad increase

--- src/hooks/useEditableTable.tsx
+++ src/hooks/useEditableTable.tsx
@@ -1,33 +1,47 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from "react";
 
 const useEditableTable = (initialData: any) => {
-    const [data, setData] = useState(initialData);
-    const prevInitialDataRef = useRef(initialData)
+  const [data, setData] = useState(initialData);
+  const prevInitialDataRef = useRef(initialData);
 
+  useEffect(() => {
+    if (
+      JSON.stringify(prevInitialDataRef.current) !== JSON.stringify(initialData)
+    ) {
+      setData((prevData: any) => {
+        const newItems = initialData.filter(
+          (newItem: any) =>
+            !prevData.some(
+              (existingItem: any) => existingItem.key === newItem.key
+            )
+        );
+        return [...prevData, ...newItems];
+      });
+      prevInitialDataRef.current = initialData;
+    }
+  }, [initialData]);
 
-    useEffect(() => {
-        if (JSON.stringify(prevInitialDataRef.current) !== JSON.stringify(initialData)) {
-            setData((prevData: any) => {
-                const newItems = initialData.filter((newItem: any) =>
-                    !prevData.some((existingItem: any) => existingItem.key === newItem.key)
-                );
-                return [...prevData, ...newItems];
-            });
-            prevInitialDataRef.current = initialData;
+  const handleValueChange = (key: any, field: any, value: any) => {
+    setData((prevData: any) => {
+      const newData = prevData.map((item: any) => {
+        if (item.key === key) {
+          const updates = { ...item, [field]: value };
+
+          // Handle cantidad updates
+          if (field === "cantidad" && item.utilidad) {
+            const utilidadPerUnit = item.utilidad / item.cantidad;
+            updates.utilidad = Number((utilidadPerUnit * value).toFixed(2));
+          }
+
+          return updates;
         }
-    }, [initialData]);
+        return item;
+      });
+      return newData;
+    });
+  };
 
-    const handleValueChange = (key: any, field: any, value: any) => {
-        const newData = data.map((item: any) => {
-            if (item.key === key) {
-                return { ...item, [field]: value };
-            }
-            return item;
-        });
-        setData(newData);
-    };
-
-    return [data, setData, handleValueChange];
+  return [data, setData, handleValueChange];
 };
 
 export default useEditableTable;
