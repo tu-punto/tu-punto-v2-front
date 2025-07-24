8bb3a4591e3384a15db89473f1ee88c42cd5b3a4
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2025-07-24 18:37:31 -0400

    feat: Add filter function for seller branch view

--- src/pages/Branch/BranchTable.tsx
+++ src/pages/Branch/BranchTable.tsx
@@ -1,8 +1,9 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Button, Table, Tooltip } from "antd";
 import { IBranch } from "../../models/branchModel";
 import { EditOutlined, MessageFilled } from "@ant-design/icons";
 import { UserContext } from "../../context/userContext";
+import { getSellerAPI } from "../../api/seller";
 
 interface BranchTableProps {
   refreshKey: number;
@@ -16,12 +17,52 @@ const BranchTable: React.FC<BranchTableProps> = ({
   showEditModal,
 }) => {
   const { user } = useContext(UserContext);
+  const [filteredBranches, setFilteredBranches] = useState<IBranch[]>(branches);
 
   const handleChatBranch = (branch: IBranch) => {
     const phoneNumber = branch.telefono;
     const whatsappUrl = `https://wa.me/${phoneNumber}`;
     window.open(whatsappUrl, '_blank');
   }
+
+  useEffect(() => {
+    if (user.role == "admin") {
+      setFilteredBranches(branches);
+    } else if (user.role == "seller") {
+      filterSellerBranchs();
+    }
+  },[branches]);
+
+  const filterSellerBranchs = async () => {
+    const sellerData = await getSellerAPI(user.id_vendedor);
+    const branchData = sellerData.pago_sucursales;
+    const activeBranchIDs: Set<number> = new Set();
+    const sellerFinalDate = new Date(sellerData.fecha_vigencia);
+    branchData.forEach((branch) => {
+      if(isActiveSellerBranch(branch, sellerFinalDate)) {
+        activeBranchIDs.add(branch.id_sucursal);
+      }
+    });
+
+    const filteredBranches: IBranch[] = [];
+    branches.forEach((branch: IBranch) => {
+      if(activeBranchIDs.has(branch._id)) {
+        filteredBranches.push(branch);
+      }
+    })
+    setFilteredBranches(filteredBranches);
+  }
+
+  const isActiveSellerBranch = (branch: any, finalDate: Date) => {
+    const actualDate = new Date();
+    let branchFinalDate: Date;
+    if (branch.fecha_salida) {
+      branchFinalDate = new Date(branch.fecha_salida);
+    } else {
+      branchFinalDate = finalDate;
+    }
+    return branchFinalDate > actualDate
+  }   
 
   const cols = [
     {
@@ -73,7 +114,7 @@ const BranchTable: React.FC<BranchTableProps> = ({
             <Tooltip title="Contactar sucursal">
               <Button
                 type="default"
-                onClick={()=>handleChatBranch(branch)}
+                onClick={() => handleChatBranch(branch)}
                 icon={<MessageFilled />}
                 className="text-mobile-sm xl:text-desktop-sm"
               />
@@ -88,7 +129,7 @@ const BranchTable: React.FC<BranchTableProps> = ({
     <Table
       key={refreshKey}
       columns={cols}
-      dataSource={branches}
+      dataSource={filteredBranches}
       pagination={{ pageSize: 10 }}
       scroll={{ x: "max-content" }}
     />
