6a468c0e33a235e1535753356ba227271f1023ff
Khenya <abrendakhenya@gmail.com>
2025-07-24 20:41:05 -0400

    refactor: SellerFacturaButton

--- src/pages/Stats/StatsPage.tsx
+++ src/pages/Stats/StatsPage.tsx
@@ -1,24 +1,43 @@
 import StatisticsDashboard from "./StatsDashboard";
-import { Outlet } from "react-router-dom";
-import SellerFacturaButton from "../../components/SellerFacturaButton";
+import SidebarNavButton  from "../../components/SidebarNavButton";
+import sellerIcon from "../../assets/sellersIcon.svg";
+
 import { useState } from "react";
+
 const StatsPage = () => {
-    const [isOpen, setIsOpen] = useState(true);
-    return (<div>
-        <StatisticsDashboard />
-        <SellerFacturaButton isOpen={isOpen} />
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div>
+      <StatisticsDashboard />
+      <div className="p-2 space-y-2">
+        <SidebarNavButton 
+          to="/sellerFactura"
+          description="Detalle Vendedores"
+          icon={sellerIcon}
+          isOpen={isOpen}
+        />
+        <SidebarNavButton 
+          to="/sellerFactura"
+          description="Desglose de servicios"
+          icon={sellerIcon}
+          isOpen={isOpen}
+        />
+        {/* <Link
+          to="/services"
+          className="flex items-center p-4 bg-blue hover:bg-light-blue/10 transition-colors duration-200"
+          key="/breakdownServices"
+        >
+          <img src={sellerIcon} alt="Desglose de servi" className="w-6 h-6 mx-3" />
+          {isOpen && (
+            <span className="ml-2 text-mobile-sm xl:text-desktop-sm whitespace-normal break-words text-left">
+              Desglose de servicios
+            </span>
+          )}
+        </Link> */}
+      </div>
     </div>
 
-    );
-    //const [isOpen, setIsOpen] = useState(true);
-    //return (
-    //    <div className="p-4 space-y-4">
-    //        {/* Contenido de estadísticas */}
-    //        <h1 className="text-xl font-bold">Dashboard de Estadísticas</h1>
-    //        <StatisticsDashboard />
-    //        {/* Aquí agregas el botón */}
-    //        <SellerFacturaButton isOpen={isOpen} />
-    //    </div>
-    //);
+  );
 };
 export default StatsPage;
