83d72933beda488bd194a82ebf9847c4037b8ad0
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2025-07-26 18:58:51 -0400

    fix: Suggested changes on BranchTable UI

--- src/pages/Branch/BranchTable.tsx
+++ src/pages/Branch/BranchTable.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { Button, Table, Tooltip } from "antd";
 import { IBranch } from "../../models/branchModel";
-import { EditOutlined, MessageFilled } from "@ant-design/icons";
+import { EditOutlined, WhatsAppOutlined } from "@ant-design/icons";
 import { UserContext } from "../../context/userContext";
 import { getSellerAPI } from "../../api/seller";
 
@@ -18,6 +18,7 @@ const BranchTable: React.FC<BranchTableProps> = ({
 }) => {
   const { user } = useContext(UserContext);
   const [filteredBranches, setFilteredBranches] = useState<IBranch[]>(branches);
+  const [tableColumns, setTableColumns] = useState<any>([]);
 
   const handleChatBranch = (branch: IBranch) => {
     const phoneNumber = branch.telefono;
@@ -31,7 +32,7 @@ const BranchTable: React.FC<BranchTableProps> = ({
     } else if (user.role == "seller") {
       filterSellerBranchs();
     }
-  },[branches]);
+  }, [branches]);
 
   const filterSellerBranchs = async () => {
     const sellerData = await getSellerAPI(user.id_vendedor);
@@ -39,14 +40,14 @@ const BranchTable: React.FC<BranchTableProps> = ({
     const activeBranchIDs: Set<number> = new Set();
     const sellerFinalDate = new Date(sellerData.fecha_vigencia);
     branchData.forEach((branch) => {
-      if(isActiveSellerBranch(branch, sellerFinalDate)) {
+      if (isActiveSellerBranch(branch, sellerFinalDate)) {
         activeBranchIDs.add(branch.id_sucursal);
       }
     });
 
     const filteredBranches: IBranch[] = [];
     branches.forEach((branch: IBranch) => {
-      if(activeBranchIDs.has(branch._id)) {
+      if (activeBranchIDs.has(branch._id)) {
         filteredBranches.push(branch);
       }
     })
@@ -62,7 +63,18 @@ const BranchTable: React.FC<BranchTableProps> = ({
       branchFinalDate = finalDate;
     }
     return branchFinalDate > actualDate
-  }   
+  }
+
+  useEffect(() => {
+    if (user.role == "admin") {
+      setTableColumns([...cols, ...adminCols]);
+    }else if (user.role == "seller") {
+      setTableColumns([...cols, ...sellerCols])
+    }else {
+      setTableColumns(cols)
+    }
+  });
+
 
   const cols = [
     {
@@ -88,47 +100,57 @@ const BranchTable: React.FC<BranchTableProps> = ({
       dataIndex: "telefono",
       key: "telefono",
       className: "text-mobile-sm xl:text-desktop-sm",
-    },
+    }
+  ];
+
+  const adminCols = [
     {
       title: "Acciones",
       key: "actions",
       width: "10%",
-      className: "text-mobile-sm flex xl:text-desktop-sm",
-      render: (_: any, branch: IBranch) => {
-        if (user.role == "admin") {
-          return (
-            <Tooltip title="Editar sucursal">
-              <Button
-                type="default"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  showEditModal(branch);
-                }}
-                icon={<EditOutlined />}
-                className="text-mobile-sm xl:text-desktop-sm"
-              />
-            </Tooltip>
-          )
-        } else if (user.role == "seller") {
-          return (
-            <Tooltip title="Contactar sucursal">
-              <Button
-                type="default"
-                onClick={() => handleChatBranch(branch)}
-                icon={<MessageFilled />}
-                className="text-mobile-sm xl:text-desktop-sm"
-              />
-            </Tooltip>
-          )
-        }
-      }
-    },
+      className: "text-mobile-sm xl:text-desktop-sm",
+      render: (_: any, branch: IBranch) => (
+        <Tooltip title="Editar sucursal">
+          <Button
+            type="default"
+            onClick={(e) => {
+              e.stopPropagation();
+              showEditModal(branch);
+            }}
+            icon={<EditOutlined />}
+            className="text-mobile-sm xl:text-desktop-sm"
+          />
+        </Tooltip>
+      )
+    }
+  ];
+
+  const sellerCols = [
+    {
+      title: "Contactar",
+      key: "contact",
+      width: "10%",
+      className: "text-mobile-sm xl:text-desktop-sm",
+      render: (_: any, branch: IBranch) => (
+        <Tooltip title="Escribir por WhatsApp">
+          <Button
+            type="default"
+            onClick={() => handleChatBranch(branch)}
+            icon={<WhatsAppOutlined />}
+            style={{
+              backgroundColor: '#25D366',
+              color: 'white'
+            }}
+          />
+        </Tooltip>
+      )
+    }
   ];
 
   return (
     <Table
       key={refreshKey}
-      columns={cols}
+      columns={tableColumns}
       dataSource={filteredBranches}
       pagination={{ pageSize: 10 }}
       scroll={{ x: "max-content" }}
