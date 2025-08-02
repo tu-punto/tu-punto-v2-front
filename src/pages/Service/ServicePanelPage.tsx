9d5df2dac1d1eb23d97c679c3caa5b17d1d72f69
Khenya <abrendakhenya@gmail.com>
2025-08-02 12:37:16 -0400

    feat: icon services

--- src/pages/Service/ServicePanelPage.tsx
+++ src/pages/Service/ServicePanelPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import ServiciosResumenTable from "./components/ServicesSummaryTable";
 import { getServicesSummaryAPI } from "../../api/services";
 import { getAllSucursalsAPI } from "../../api/sucursal";
+import servicesIcon from "../../assets/services2.png";
 
 export const ServicePanelPage: React.FC<{ isFactura: boolean }> = () => {
   const [summary, setSummary] = useState<any | null>(null);
@@ -13,7 +14,11 @@ export const ServicePanelPage: React.FC<{ isFactura: boolean }> = () => {
       try {
         const { success, data, message } = await getAllSucursalsAPI();
         if (success) {
-          setSucursals(data.map((s: any) => s.nombre || s.name || s.sucursalName));
+          const activas = data
+            .filter((s: any) => s.activa === true) 
+            .map((s: any) => s.nombre || s.name || s.sucursalName);
+
+          setSucursals(activas);
         } else {
           console.error("Error al obtener sucursales:", message);
         }
@@ -40,7 +45,7 @@ export const ServicePanelPage: React.FC<{ isFactura: boolean }> = () => {
   return (
     <div className="p-4">
       <div className="flex items-center gap-3 bg-white rounded-xl px-5 py-2 shadow-md mb-4">
-        <img src="/config-icon.png" alt="Vendedores" className="w-8 h-8" />
+        <img src={servicesIcon} alt="Servicios" className="w-16" />
         <h1 className="text-mobile-3xl xl:text-desktop-3xl font-bold text-gray-800">
           PANEL DE CONTROL DE SERVICIOS
         </h1>
