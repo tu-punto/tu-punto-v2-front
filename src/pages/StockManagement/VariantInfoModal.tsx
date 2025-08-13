0341541aa3eb5b86d7f696564a89873244610b47
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2025-08-13 10:04:56 -0400

    feat: add entry table for variant stock modal

--- src/pages/StockManagement/VariantInfoModal.tsx
+++ src/pages/StockManagement/VariantInfoModal.tsx
@@ -1,38 +1,52 @@
-import { Modal, Table, Button, message, Card } from 'antd';
-import { useEffect, useState } from 'react';
+import { Modal, Table, message, Card } from 'antd';
+import { useEffect, useState, useContext } from 'react';
+import { UserContext } from '../../context/userContext';
 import { getSucursalsAPI } from '../../api/sucursal';
+import { getProductsEntryAmount } from '../../api/entry';
+import moment from 'moment-timezone';
 
 const VariantInfoModal = ({ visible, onClose, rowRecord }: {
     visible: boolean;
     onClose: () => void;
     rowRecord?: any;
 }) => {
-    const [isSelectedRow, setIsSelectedRow] = useState(false)
+    const { user }: any = useContext(UserContext);
     const [productName, setProductName] = useState<string | null>(null);
     const [variantName, setVariantName] = useState<string | null>(null);
     const [stockData, setStockData] = useState<any[]>();
     const [salesData, setSalesData] = useState<any[]>();
-    const [ingData, setIngData] = useState<any[]>();
-    const [branchMap, setBranchMap] = useState<Map<string,string>>(new Map);
+    const [entryData, setEntryData] = useState<any[]>();
+    const [branchMap, setBranchMap] = useState<Map<string, string>>(new Map);
+    const [entrySet, setEntrySet] = useState<Set<string>>(new Set);
+    const [entryRawData, setEntryRawData] = useState<any[]>();
 
     useEffect(() => {
         fetchBranches();
+        fetchEntries();
+    }, [rowRecord])
+
+    useEffect(() => {
         if (!rowRecord) return;
+
+        if (rowRecord.ingreso) {
+            setEntrySet(new Set(rowRecord.ingreso));
+        }
         if (rowRecord.nombre_producto) {
-            setProductName(rowRecord.nombre_producto)
+            setProductName(rowRecord.nombre_producto);
         }
         if (rowRecord.variant) {
-            setVariantName(rowRecord.variant)
+            setVariantName(rowRecord.variant);
         }
+
         cleanStockData();
         cleanSalesData();
-        cleanIngData();
-    }, [rowRecord])
+        cleanEntryData();
+    }, [rowRecord, entrySet])
 
     const fetchBranches = async () => {
         try {
             const branchData = await getSucursalsAPI();
-            if(!branchData) return
+            if (!branchData) return
 
             const map = new Map<string, string>();
             branchData.forEach((branch: any) => {
@@ -45,15 +59,25 @@ const VariantInfoModal = ({ visible, onClose, rowRecord }: {
         }
     }
 
+    const fetchEntries = async () => {
+        try {
+            const entriesData = await getProductsEntryAmount(user.id_vendedor);
+            if (!entriesData) return
+            setEntryRawData(entriesData);
+        } catch (error) {
+            console.error("Error al obtener ingresos", error)
+        }
+    }
+
     const cleanStockData = () => {
-        const cleanData: {nombre_sucursal: string, stock: number}[] = [];
+        const cleanData: { nombre_sucursal: string, stock: number }[] = [];
         const branches = rowRecord.sucursales;
         branches.forEach((branch: any) => {
             branch.combinaciones.forEach((b: any) => {
                 if (b.variantes["Variante 1"] == rowRecord.variant) {
                     const nombre_sucursal = branchMap.get(branch.id_sucursal) || "";
                     const stock = b.stock;
-                    cleanData.push({nombre_sucursal, stock})
+                    cleanData.push({ nombre_sucursal, stock })
                 }
             })
         });
@@ -64,8 +88,21 @@ const VariantInfoModal = ({ visible, onClose, rowRecord }: {
 
     }
 
-    const cleanIngData = () => {
-
+    const cleanEntryData = async () => {
+        if (entrySet.size == 0) {
+            console.log("ya ni le muevas pa")
+        }
+        const cleanData: { fecha: Date, sucursal: string, producto: string, cantidad: number }[] = [];
+        entryRawData?.forEach((entry: any) => {
+            if (entrySet.has(entry._id)) {
+                const fecha = entry.fecha_ingreso;
+                const sucursal = branchMap.get(entry.sucursal) || "";
+                const producto = entry.nombre_variante;
+                const cantidad = entry.cantidad_ingreso;
+                cleanData.push({ fecha, sucursal, producto, cantidad })
+            }
+        })
+        setEntryData(cleanData);
     }
 
     const stockColumns = [
@@ -112,11 +149,13 @@ const VariantInfoModal = ({ visible, onClose, rowRecord }: {
             dataIndex: 'subtotal',
         },
     ];
-    const ingColumns = [
+    const entryColumns = [
         {
             title: 'Fecha',
             key: 'fecha',
             dataIndex: 'fecha',
+            render: (text: string) =>
+                moment.parseZone(text).format("DD/MM/YYYY"),
         },
         {
             title: 'Sucursal',
@@ -141,7 +180,6 @@ const VariantInfoModal = ({ visible, onClose, rowRecord }: {
     }
 
     const resetFields = () => {
-        setIsSelectedRow(false);
         setProductName(null);
         setVariantName(null);
     }
@@ -170,11 +208,12 @@ const VariantInfoModal = ({ visible, onClose, rowRecord }: {
             </Card>
             <Card title="Historial de ingresos" bordered={false}>
                 <Table
-                    columns={ingColumns}
-                    dataSource={ingData}
+                    columns={entryColumns}
+                    dataSource={entryData}
                     pagination={{ pageSize: 15 }}
                 />
             </Card>
+
         </Modal>
     );
 };
