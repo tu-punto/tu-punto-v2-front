b3ae14a833270c5f1a4294b7c71f2349088ff163
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2025-08-15 13:09:33 -0400

    feat: add range hour option to edit shipping modal

--- src/pages/Shipping/ShippingInfoModal.tsx
+++ src/pages/Shipping/ShippingInfoModal.tsx
@@ -2,7 +2,7 @@ import TempProductModal from './TempProductModal';
 import { useEffect, useState, useMemo } from 'react';
 import {
     Modal, Card, Button, Form, Input, DatePicker, Row, Col, TimePicker,
-    Radio, Select, InputNumber, message
+    Radio, Select, InputNumber, message, Switch
 } from 'antd';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
@@ -38,6 +38,7 @@ const ShippingInfoModal = ({ visible, onClose, shipping, onSave, sucursals = [],
     const [estadoPedido, setEstadoPedido] = useState<string | null>(null);
     const [tipoPago, setTipoPago] = useState<string | null>(null);
     const [estadoInicialPedido, setEstadoInicialPedido] = useState<string | null>(null);
+    const [isRangeHour, setIsRangeHour] = useState(false);
     const [qrInput, setQrInput] = useState<number>(0);
     const [efectivoInput, setEfectivoInput] = useState<number>(0);
     const [showWarning, setShowWarning] = useState(false);
@@ -162,6 +163,7 @@ const ShippingInfoModal = ({ visible, onClose, shipping, onSave, sucursals = [],
             internalForm.setFieldsValue({
                 fecha_entrega: ahora,
                 hora_entrega_acordada: ahora,
+                hora_entrega_rango_final: ahora
             });
         }
     }, [estadoPedidoForm, estadoInicialPedido]);
@@ -199,6 +201,8 @@ const ShippingInfoModal = ({ visible, onClose, shipping, onSave, sucursals = [],
         const rawFecha = shipping.hora_entrega_acordada;
         const originalHoraEntregaUTC = rawFecha ? dayjs.utc(rawFecha) : null;
 
+        setIsRangeHour(shipping.hora_entrega_rango_final)
+
         //console.log("🟢 UTC:", dayjs.utc(shipping.hora_entrega_acordada).format());
         //console.log("🟡 Local:", dayjs.utc(shipping.hora_entrega_acordada).local().format());
 
@@ -211,6 +215,7 @@ const ShippingInfoModal = ({ visible, onClose, shipping, onSave, sucursals = [],
             hora_entrega_acordada: originalHoraEntregaUTC
                 ? dayjs(originalHoraEntregaUTC.format("HH:mm:ss"), "HH:mm:ss")
                 : null,
+            hora_entrega_rango_final: shipping.hora_entrega_rango_final ? dayjs.utc(shipping.hora_entrega_rango_final, "HH:mm:ss") : null,
             observaciones: shipping.observaciones,
             estado_pedido: shipping.estado_pedido,
             quien_paga_delivery: quienPagaDeVenta,
@@ -446,13 +451,20 @@ const ShippingInfoModal = ({ visible, onClose, shipping, onSave, sucursals = [],
                 ...values,
                 lugar_entrega: values.lugar_entrega === 'otro' ? values.lugar_entrega_input : values.lugar_entrega,
                 //fecha_pedido: moment(values.fecha_pedido).tz("America/La_Paz").format('YYYY-MM-DD HH:mm:ss'),
-                hora_entrega_acordada: fechaHoraEntregaAcordada,                pagado_al_vendedor: values.esta_pagado === 'si' || values.tipo_de_pago === '3',
+                hora_entrega_acordada: fechaHoraEntregaAcordada,                
+                pagado_al_vendedor: values.esta_pagado === 'si' || values.tipo_de_pago === '3',
+                //hora_entrega_rango_final: horaEntregaRangoFinal,
                 esta_pagado: values.esta_pagado,
                 adelanto_cliente: ['si', 'no'].includes(values.esta_pagado) ? 0 : (values.adelanto_cliente || 0),
                 quien_paga_delivery: values.quien_paga_delivery,
             };
             if (values.quien_paga_delivery === "tupunto") {
                 updateShippingInfo.cargo_delivery = 0;
+            }
+            if (values.hora_entrega_rango_final) {
+                //const hora = values.hora_entrega_rango_final.format("HH:mm:ss");
+                const hora = `${fechaEntrega.format("YYYY-MM-DD")} ${values.hora_entrega_rango_final.format("HH:mm:ss")}`;
+                updateShippingInfo.hora_entrega_rango_final = hora
             }
 
             // Forzar subtotales según el tipo de pago
@@ -475,7 +487,7 @@ const ShippingInfoModal = ({ visible, onClose, shipping, onSave, sucursals = [],
                     break;
             }
 
-            //console.log("📤 Datos enviados al backend:", updateShippingInfo);
+            console.log("📤 Datos enviados al backend:", updateShippingInfo);
 
             await updateShippingAPI(updateShippingInfo, shipping._id);
             message.success("Pedido actualizado con éxito");
@@ -630,11 +642,37 @@ const ShippingInfoModal = ({ visible, onClose, shipping, onSave, sucursals = [],
                                 <DatePicker style={{ width: '100%' }} />
                             </Form.Item>
                         </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        {shipping?.estado_pedido !== "Entregado" && (
+                            <div style={{ margin:8 }}>
+                            <span style={{ marginRight:8 }}>
+                                - ¿Acordar entrega en un rango de horas?
+                            </span>
+                            <Switch
+                                checked={isRangeHour}
+                                disabled={shipping?.estado_pedido === "Entregado"}
+                                onChange={(checked) => { setIsRangeHour(checked); }}
+                                unCheckedChildren="Hora específica"
+                                checkedChildren="Rango de horas"
+                            />
+                        </div>
+                        )}
                         <Col span={12}>
-                            <Form.Item name="hora_entrega_acordada" label="Hora Entrega">
+                            <Form.Item 
+                                name="hora_entrega_acordada" 
+                                label={isRangeHour? "Inicio del Rango Horario":"Hora de Entrega Específica"}
+                            >
                                 <TimePicker format='HH:mm' style={{ width: '100%' }} />
                             </Form.Item>
                         </Col>
+                        {isRangeHour && shipping?.estado_pedido !== "Entregado" && (
+                            <Col span={12}>
+                                <Form.Item name="hora_entrega_rango_final" label="Fin del Rango Horario">
+                                    <TimePicker format='HH:mm' style={{ width: '100%' }} />
+                                </Form.Item>
+                            </Col>
+                        )}
                     </Row>
                     <Row gutter={16}>
                         <Col span={24}>
