e0f5371f4dda3fcf1e0580d065a1f4f55054ae54
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2026-01-12 12:55:37 -0400

    update product seller form modal

--- src/pages/Seller/ProductSellerViewModal.tsx
+++ src/pages/Seller/ProductSellerViewModal.tsx
@@ -1,12 +1,13 @@
 import { Select, Button, Form, Input, Modal, message, InputNumber } from "antd"
 import { useContext, useEffect, useState } from "react"
-import { getCategoriesAPI} from "../../api/category"
+import { getCategoriesAPI } from "../../api/category"
 import { UserContext } from "../../context/userContext";
 import { registerVariantAPI } from "../../api/product";
 import { createEntryAPI } from "../../api/entry";
 import { registerProductAPI } from "../../api/product";
+import FormModal from "../../components/FormModal";
 
-const ProductSellerViewModal = ({ visible, onCancel, onSuccess, onAddProduct, selectedSeller, openFromEditProductsModal = false, sellers = [] , sucursalId }: any) => {
+const ProductSellerViewModal = ({ visible, onCancel, onSuccess, onAddProduct, selectedSeller, openFromEditProductsModal = false, sellers = [], sucursalId }: any) => {
     const { user }: any = useContext(UserContext);
     const [loading, setLoading] = useState(false)
     const [categories, setCategories] = useState([])
@@ -99,30 +100,100 @@ const ProductSellerViewModal = ({ visible, onCancel, onSuccess, onAddProduct, se
     }, [visible]);
 
     return (
-        <Modal
-            title="Agregar Producto"
-            open={visible}
-            onCancel={onCancel}
-            footer={null}
-            width={800}
-        >
-            <Form
-                form={form}
-                name="productForm"
+        <>
+            <FormModal
+                title="Agregar Producto"
+                open={visible}
+                onClose={onCancel}
+                submitTitle="Registrar Producto"
                 onFinish={handleFinish}
-                layout="vertical"
+                form={form}
             >
-                {openFromEditProductsModal ? (
+                <>
+                    {openFromEditProductsModal ? (
+                        <Form.Item
+                            name="id_vendedor"
+                            label="Vendedor"
+                            rules={[{ required: true, message: 'Seleccione un vendedor' }]}
+                        >
+                            <Select
+                                placeholder="Selecciona un vendedor"
+                                options={sellers.map((s: any) => ({
+                                    value: s._id,
+                                    label: `${s.nombre} ${s.apellido}`
+                                }))}
+                                showSearch
+                                filterOption={(input, option: any) =>
+                                    option.label.toLowerCase().includes(input.toLowerCase())
+                                }
+                            />
+                        </Form.Item>
+                    ) : (
+                        <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: 16 }}>
+                            <span style={{
+                                backgroundColor: '#f0f2f5',
+                                padding: '6px 12px',
+                                borderRadius: 8,
+                                fontWeight: 'bold'
+                            }}>
+                                Vendedor: {selectedSeller?.nombre} {selectedSeller?.apellido}
+                            </span>
+                        </div>
+                    )}
                     <Form.Item
-                        name="id_vendedor"
-                        label="Vendedor"
-                        rules={[{ required: true, message: 'Seleccione un vendedor' }]}
+                        name="nombre_producto"
+                        label="Nombre del Producto"
+                        rules={[{ required: true, message: 'Por favor ingrese el name del producto' }]}
+                    >
+                        <Input placeholder="Nombre del Producto" />
+                    </Form.Item>
+                    <Form.Item
+                        name="precio"
+                        label="Precio del producto"
+                        rules={[
+                            { required: true, message: 'Por favor ingrese el precio del producto' },
+                            { type: 'number', min: 0.01, message: 'El precio debe ser mayor a 0' }
+                        ]}
+                    >
+                        <InputNumber
+                            suffix="Bs."
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        name="cantidad_por_sucursal"
+                        label="Cantidad de ingreso del producto"
+                        rules={[
+                            { required: true, message: 'Por favor ingrese la cantidad de ingreso del producto' },
+                            { type: 'number', min: 1, message: 'Debe ser al menos 1 unidad' }
+                        ]}
+                    >
+                        <InputNumber
+                            min={1}
+                            parser={value => value ? parseInt(value, 10) : 0}
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        name="id_categoria"
+                        label="Categoría"
+                        rules={[{ required: true, message: 'Por favor seleccione una categoría' }]}
                     >
                         <Select
-                            placeholder="Selecciona un vendedor"
-                            options={sellers.map((s: any) => ({
-                                value: s._id,
-                                label: `${s.nombre} ${s.apellido}`
+                            placeholder="Selecciona una categoría"
+                            dropdownRender={menu => (
+                                <>
+                                    {menu}
+                                    <div style={{ display: 'flex', padding: 8 }}>
+                                        <Input
+                                            style={{ flex: 'auto' }}
+                                            value={newCategory}
+                                            onChange={e => setNewCategory(e.target.value)}
+                                        />
+                                    </div>
+                                </>
+                            )}
+                            options={categories.map((category: any) => ({
+                                value: category._id,
+                                label: category.categoria,
                             }))}
                             showSearch
                             filterOption={(input, option: any) =>
@@ -130,120 +201,8 @@ const ProductSellerViewModal = ({ visible, onCancel, onSuccess, onAddProduct, se
                             }
                         />
                     </Form.Item>
-                ) : (
-                    <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: 16 }}>
-      <span style={{
-          backgroundColor: '#f0f2f5',
-          padding: '6px 12px',
-          borderRadius: 8,
-          fontWeight: 'bold'
-      }}>
-        Vendedor: {selectedSeller?.nombre} {selectedSeller?.apellido}
-      </span>
-                    </div>
-                )}
-                <Form.Item
-                    name="nombre_producto"
-                    label="Nombre del Producto"
-                    rules={[{ required: true, message: 'Por favor ingrese el name del producto' }]}
-                >
-                    <Input placeholder="Nombre del Producto" />
-                </Form.Item>
-                <Form.Item
-                    name="precio"
-                    label="Precio del producto"
-                    rules={[
-                        { required: true, message: 'Por favor ingrese el precio del producto' },
-                        { type: 'number', min: 0.01, message: 'El precio debe ser mayor a 0' }
-                    ]}
-                >
-                    <InputNumber
-                        suffix="Bs."
-                    />
-                </Form.Item>
-                <Form.Item
-                    name="cantidad_por_sucursal"
-                    label="Cantidad de ingreso del producto"
-                    rules={[
-                        { required: true, message: 'Por favor ingrese la cantidad de ingreso del producto' },
-                        { type: 'number', min: 1, message: 'Debe ser al menos 1 unidad' }
-                    ]}
-                >
-                    <InputNumber
-                        min={1}
-                        parser={value => value ? parseInt(value, 10) : 0}
-                    />
-                </Form.Item>
-                {/* <Form.Item
-                    name='sucursal'
-                    label="Sucursal"
-                    rules={[{ required: true, message: 'Por favor seleccione una sucursal' }]}
-                >
-                    <Select
-                        placeholder='Selecciona una sucursal'
-                        options={branches.map((branch: any) => ({
-                            value: branch.id_sucursal,
-                            label: branch.nombre
-                        }))}
-                        showSearch
-                        filterOption={(input, option: any) =>
-                            option.label.toLocaleLowerCase().includes(input.toLocaleLowerCase())}
-                    />
-
-                </Form.Item> */}
-                {/* <Form.Item
-                    name="id_vendedor"
-                    label="Marca"
-                    rules={[{ required: true, message: 'Por favor seleccione una marca' }]}
-                >
-                    <Select
-                        placeholder="Selecciona una marca"
-                        options={sellers.map((seller: any) => ({
-                            value: seller.id_vendedor,
-                            label: seller.marca,
-                        }))}
-                        showSearch
-                        filterOption={(input, option: any) =>
-                            option.label.toLowerCase().includes(input.toLowerCase())
-                        }
-                    />
-                </Form.Item> */}
-                <Form.Item
-                    name="id_categoria"
-                    label="Categoría"
-                    rules={[{ required: true, message: 'Por favor seleccione una categoría' }]}
-                >
-                    <Select
-                        placeholder="Selecciona una categoría"
-                        dropdownRender={menu => (
-                            <>
-                                {menu}
-                                <div style={{ display: 'flex', padding: 8 }}>
-                                    <Input
-                                        style={{ flex: 'auto' }}
-                                        value={newCategory}
-                                        onChange={e => setNewCategory(e.target.value)}
-                                    />
-                                </div>
-                            </>
-                        )}
-                        options={categories.map((category: any) => ({
-                            value: category._id,
-                            label: category.categoria,
-                        }))}
-                        showSearch
-                        filterOption={(input, option: any) =>
-                            option.label.toLowerCase().includes(input.toLowerCase())
-                        }
-                    />
-                </Form.Item>
-        
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" loading={loading} className="text-mobile-sm xl:text-desktop-sm">
-                        Registrar Producto
-                    </Button>
-                </Form.Item>
-            </Form>
+                </>
+            </FormModal>
             <Modal
                 title="Confirmación"
                 open={showConfirm}
@@ -257,9 +216,8 @@ const ProductSellerViewModal = ({ visible, onCancel, onSuccess, onAddProduct, se
             >
                 ¿Está seguro de que desea registrar este producto?
             </Modal>
-
-        </Modal>
-    );
+        </>
+    )
 };
 
 export default ProductSellerViewModal;
