aa7b10821a7bab1df838ae170f4564f479f9a9af
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2026-02-04 09:17:49 -0400

    apply LoginTemplate component on SellerLoginPage

--- src/pages/Login/SellerLoginPage.tsx
+++ src/pages/Login/SellerLoginPage.tsx
@@ -1,36 +1,15 @@
 import { useNavigate } from "react-router-dom";
-import { Button } from "antd";
-
-import LoginForm from "../../components/LoginForm";
-import logoImg from "../../../public/logo.png";
+import LoginTemplate from "../../components/LoginTemplate";
 
 export default function SellerLoginPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 bg-[url('/background-login.png')] bg-cover bg-center">
-      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-        <div className="text-center">
-          <img
-            alt="logo"
-            src={logoImg}
-            className="mx-auto h-20 w-auto rounded-full"
-          />
-          <h2 className="mt-6 text-mobile-2xl xl:text-desktop-3xl font-bold text-gray-900">
-            Tu Punto (Vendedor)
-          </h2>
-        </div>
-        <LoginForm showBranchSelect={false} redirectTo="/seller-info" />
-        <div className="text-center">
-          <Button
-            type="default"
-            onClick={() => navigate("/login-admin")}
-            className="w-full"
-          >
-            Iniciar sesión como Admin
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
+    <LoginTemplate
+      title="Tu Punto (Vendedor)"
+      buttonTitle="Iniciar sesión como Admin"
+      buttonOnClick={() => navigate("/login-admin")}
+      redirectTo="/seller-info"
+    />
+  )
 }
