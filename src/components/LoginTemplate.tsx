e2730b6a4fe483548510e4f12336e866bf8d91c6
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2026-02-05 12:11:12 -0400

    update LoginTemplate Style, add divider

--- src/components/LoginTemplate.tsx
+++ src/components/LoginTemplate.tsx
@@ -1,4 +1,4 @@
-import { Button } from "antd";
+import { Button, Divider } from "antd";
 import logoImg from "/logo.png";
 import LoginForm from "./LoginForm";
 
@@ -13,7 +13,7 @@ interface LoginTemplateProps {
 function LoginTemplate({ title, buttonTitle, buttonOnClick, showBranchSelect = false, redirectTo }: LoginTemplateProps) {
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100 bg-[url('/background-login.png')] bg-cover bg-center">
-            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+            <div className="w-full max-w-md p-8 space-y-4 bg-white rounded-lg shadow-md">
                 <div className="text-center">
                     <img
                         alt="logo"
@@ -23,19 +23,20 @@ function LoginTemplate({ title, buttonTitle, buttonOnClick, showBranchSelect = f
                     <h2 className="mt-6 text-mobile-2xl xl:text-desktop-3xl font-bold text-gray-900">
                         {title}
                     </h2>
+                </div>
 
-                    {/* LoginForm revisar carga de sucursales - error */}
-                    <LoginForm showBranchSelect={showBranchSelect} redirectTo={redirectTo} />
+                <LoginForm showBranchSelect={showBranchSelect} redirectTo={redirectTo} />
 
-                    <div className="text-center">
-                        <Button
-                            type="default"
-                            onClick={buttonOnClick}
-                            className="w-full"
-                        >
-                            {buttonTitle}
-                        </Button>
-                    </div>
+                <Divider style={{ borderColor: '#bbbbbb' }}>o</Divider>
+
+                <div className="text-center">
+                    <Button
+                        type="default"
+                        onClick={buttonOnClick}
+                        className="w-full"
+                    >
+                        {buttonTitle}
+                    </Button>
                 </div>
             </div>
         </div>
