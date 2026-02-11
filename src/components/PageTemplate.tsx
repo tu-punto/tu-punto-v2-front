db2adcc68c9109733f157b8c16097135bc520bea
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2026-02-11 13:12:37 -0400

    update actions section to be responsive, add divider

--- src/components/PageTemplate.tsx
+++ src/components/PageTemplate.tsx
@@ -18,21 +18,21 @@ export type FunctionButtonProps = {
 function PageTemplate({ title, iconSrc = "/box-icon.png", children, actions = [] }: PageTemplateProps) {
     return (
         <>
-            <div className="flex justify-between items-center mb-4 p-4">
+            <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4 mb-4 p-4 border-b border-gray-200">
                 <div className="flex items-center gap-3 bg-white rounded-xl px-5 py-2 shadow-md">
                     <img src={iconSrc} alt={title} className="w-8 h-8" />
                     <h1 className="text-mobile-3xl xl:text-desktop-3xl font-bold text-gray-800">
                         {title}
                     </h1>
                 </div>
-                <div className="flex gap-3 items-center">
+                <div className="flex gap-3 items-center w-full md:w-auto">
                     {actions.map((act) => 
                         act.visible && (
                             <Button
                                 key={act.title}
                                 onClick={act.onClick}
                                 type="primary"
-                                className="text-mobile-sm xl:text-desktop-sm"
+                                className="text-mobile-sm xl:text-desktop-sm flex-1 md:flex-none"
                                 icon={act.icon}
                             >
                                 {act.title}
